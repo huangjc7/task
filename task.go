b79bc7615f56df55a60d2b0a45caad277bcf77dc
huangjianchen <huangjc7@foxmail.com>
2020-09-27 10:54:43 +0800

    non-fast-forward

--- task.go
+++ task.go
@@ -1,5 +1,13 @@
-package task
+package main
 
-func TestFunc() string {
-	return "hjccs"
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for {
+		fmt.Println("程序运行中...")
+		time.Sleep(time.Second * 1)
+	}
 }
